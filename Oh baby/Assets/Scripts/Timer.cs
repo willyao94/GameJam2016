04f6ddfce74cdc628c9c38f2074f0818ea5f6f26
heiyami <lou.julian96@gmail.com>
2016-01-30 19:46:08 -0800

    Added timer script

---
+++ Oh baby/Assets/Scripts/Timer.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class Timer : MonoBehaviour {
+
+    public static float maxTime = 1296000; // start countdown at this
+    private float currentTime;
+    public Text textTime; 
+
+
+    // Use this for initialization
+    void Start() {
+        currentTime = maxTime;
+        textTime = GetComponent<Text>();
+    }
+
+    // Update is called once per frame
+    void Update() {
+        currentTime += 30;
+
+        string hours = ((int)currentTime / 216000).ToString();
+        int rawMinutes = ((int) currentTime / 3600)%60;
+        string minutes = rawMinutes.ToString();
+
+        if (rawMinutes < 10) {
+            minutes = "0" + rawMinutes.ToString();
+        }
+
+        textTime.text = hours + ":" + minutes + " PM";
+    }
+}
